fb3449e5916fe73373cc6ef258e7409a06507170
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-09-02 11:05:46 -0500

    Pequeño cambio en registro

--- build/routes/authRoutes.js
+++ build/routes/authRoutes.js
@@ -5,11 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authController_1 = __importDefault(require("../controllers/authController"));
-const authMiddleware_1 = __importDefault(require("../middlewares/authMiddleware"));
-const rolValidatorMiddleware_1 = __importDefault(require("../middlewares/rolValidatorMiddleware"));
 const stateValidatorMiddleware_1 = __importDefault(require("../middlewares/stateValidatorMiddleware"));
 const router = express_1.default.Router();
-router.post('/registro', authMiddleware_1.default, (0, rolValidatorMiddleware_1.default)(['ADMINISTRADOR']), authController_1.default.registrarUsuario);
+router.post('/registro', authController_1.default.registrarUsuario);
 router.post('/login', (0, stateValidatorMiddleware_1.default)(true), authController_1.default.loginUsuario);
 router.get('/ruta-protegida', authController_1.default.saludar);
 exports.default = router;
